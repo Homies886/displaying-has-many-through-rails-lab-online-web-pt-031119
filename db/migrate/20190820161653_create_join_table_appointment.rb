1e52193bdc945b0cf148936d38068e719f34b3c2
Nicholas Rodriguez <niko876@hotmail.com>
2019-08-20 16:18:37 +0000

    Automatically backed up by Learn

--- db/migrate/20190820161653_create_join_table_appointment.rb
+++ db/migrate/20190820161653_create_join_table_appointment.rb
@@ -1,8 +1,11 @@
-class CreateJoinTableAppointment < ActiveRecord::Migration[5.0]
+class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :patients, :doctors do |t|
-      # t.index [:patient_id, :doctor_id]
-      # t.index [:doctor_id, :patient_id]
+    create_table :appointments do |t|
+      t.datetime :appointment_datetime
+      t.belongs_to :doctor, foreign_key: true
+      t.belongs_to :patient, foreign_key: true
+
+      t.timestamps
     end
   end
 end
